bb7130b7218278e6f9d2615416ecc9ba58585f36
zm1hawk <vinicius6.lopes6@gmail.com>
2023-06-02 16:44:12 -0300

    Post product

--- dscommerce/src/main/java/com/devsuperior/dscommerce/controllers/ProductController.java
+++ dscommerce/src/main/java/com/devsuperior/dscommerce/controllers/ProductController.java
@@ -5,6 +5,8 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -31,5 +33,9 @@ public class ProductController {
 		
 	}
 	
-	
+	@PostMapping
+	public ProductDTO insert(@RequestBody ProductDTO dto) {
+		
+		return service.insert(dto);
+	}
 }
